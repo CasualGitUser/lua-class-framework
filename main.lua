5ae133357920d9cb309cf9d3a3daca891b817d20
NameyName <92256193+CasualGitUser@users.noreply.github.com>
2024-09-02 21:27:59 +0200

    added polymorphism

--- main.lua
+++ main.lua
@@ -1,3 +1,32 @@
+-- rescursively compares all the self and selfSuperTypes with the type of other
+function compareClassTypes(self, other, originalSelf)
+  local selfSuper = getmetatable(self)
+  local otherSuper = getmetatable(other)
+  
+  if self.type == other.type then return true
+  elseif selfSuper then return compareClassTypes(selfSuper, other, originalSelf)
+  elseif otherSuper then return compareClassTypes(originalSelf, otherSuper, originalSelf)
+  else return false end
+end
+
+--lookup table for typeMetaTables
+local metaTypeClasses = {}
+
+-- makes the == work between a instance.type and another instance.type
+-- note: doesnt work with a instance.type == string due to limitations of the __eq metamethod
+function classTypeMeta(typeName)
+  if metaTypeClasses[typeName] then return metaTypeClasses[typeName] end
+  local meta = {}
+  meta.type = typeName
+  meta.__index = meta
+  meta.__eq = function(self, other)
+    return compareClassTypes(self, other, self)
+  end
+  metaTypeClasses[typeName] = meta
+  return meta
+end
+
+--handles the creation of classes
 function class(name)
   local classBluePrint = {}
   local meta = {}
@@ -19,16 +48,25 @@ function class(name)
   --the class body
   meta.__call = function(self, prototype)
     prototype.__index = prototype
-    --for the constructor. this is boilerplate
+    --for polymorphism and type checking
+    prototype.type = {}
+    setmetatable(prototype.type, classTypeMeta(name))
+    
+    --for the constructor. this reduces boilerplate
     prototype.__call = function(class, ...)
       local o = {}
       setmetatable(o, prototype)
       return prototype.constructor(o, ...)
     end
+    
     --funny tongue twister lays ahead
     --if a super prototype exists, set the prototype of the prototype to the super prototype
     --aka inheritance
-    if self.super then setmetatable(prototype, self.super) end
+    if self.super then
+      setmetatable(prototype, self.super)
+      setmetatable(prototype.type, self.super.type)
+    end
+    
     --overrides the current metatable (discards extends and setSuper functions) with the specified prototype
     setmetatable(classBluePrint, prototype)
     return classBluePrint
@@ -38,6 +76,18 @@ function class(name)
 end
 
 --------------------EXAMPLES--------------------
+
+local robot = class "robot" {
+  fuel = 100,
+  drive = function()
+    print("driving")
+  end,
+  constructor = function(self)
+    return self
+  end
+}
+
+local r = robot()
 
 local person = class "person" {
   age = 25,
@@ -53,8 +103,7 @@ local person = class "person" {
 }
 
 local p = person("john", 10)
-
-print(p.name, p.age)
+local p2 = person("bob", 20)
 
 local employee = class "employee" : extends(person) {
   salary = 25000,
@@ -66,6 +115,9 @@ local employee = class "employee" : extends(person) {
     return self
   end
 }
+
+local e = employee()
+e.salary = 30000
 
 local manager = class "manager" : extends(employee) {
   salary = 100000,
